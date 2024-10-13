ad8f7c2ffa992dccc3f2790a94e873eca057d09b
xxqqwua <xxqqwua@gmail.com>
2024-10-13 16:00:46 +0300

    Added a medium difficulty level with limited attempts and an increased number to guess at

--- logic.py
+++ logic.py
@@ -1,9 +1,6 @@
 import random
 
 
-number_to_guess = random.randint(1, 100)
-
-
 # print rules
 print('-'*30 + '\nRules:' + '\n' + '\n')
 print('The programme guesses a number, by answering the programme Higher or Lower, in N number of tries you have to guess '
@@ -15,10 +12,31 @@ def hard_level():
 
 
 def medium_level():
-    pass
+    number_to_guess = random.randint(1, 200)
+    numbers_of_tries = 0
+
+    if number_to_guess > 100:
+        print('First hint: The number is higher than 100')
+    else:
+        print('First hint: The number is lower than 100')
+
+    while numbers_of_tries < 10:
+        user_guess = int(input('Enter your guess: '))
+
+        if user_guess < number_to_guess:
+            print('Higher')
+        elif user_guess > number_to_guess:
+            print('Lower')
+        else:
+            print('You guessed it!')
+            break
+        numbers_of_tries += 1
+        print(f'You have {11 - numbers_of_tries} tries left')
 
 
 def easy_level():
+    number_to_guess = random.randint(1, 100)
+
     if number_to_guess > 50:
         print('First hint: The number is higher than 50')
     else:
