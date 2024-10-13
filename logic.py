7601eed9b2cc5ffa257fd42173490d7c367f4e70
xxqqwua <xxqqwua@gmail.com>
2024-10-13 15:35:45 +0300

    add rules

---
+++ logic.py
@@ -0,0 +1,10 @@
+import random
+
+number_to_guess = random.randint(1, 100)
+
+print('-'*30 + '\nRules:' + '\n' + '\n') # print rules
+print('The programme guesses a number, by answering the programme cold or hot, in N number of tries you have to guess '
+      'the number' + '\n' + '-'*30)
+
+# while True:
+
